be97c44e2f5b1d3c6f3a6487da1b96025dd22e61
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-05-02 16:18:27 -0700

    Minor refactoring

--- AudioFile.h
+++ AudioFile.h
@@ -1,21 +1,33 @@
 //
-//  AudioFile.h
-//  AudioBookBinder
+//  Copyright (c) 2010-2016 Oleksandr Tymoshenko <gonzo@bluezbox.com>
+//  All rights reserved.
+// 
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//  1. Redistributions of source code must retain the above copyright
+//     notice unmodified, this list of conditions, and the following
+//     disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//  SUCH DAMAGE.
 //
-//  Created by Oleksandr Tymoshenko on 10-02-06.
-//  Copyright 2010 Bluezbox Software. All rights reserved.
-//
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
-
-@interface AudioFile : NSObject {
-    NSString *filePath;
-    NSString *file;
-    NSNumber *duration;
-    NSString *artist, *name, *album;
-    BOOL valid;
-}
+@interface AudioFile : NSObject
 
 @property(readwrite, copy) NSString *filePath;
 @property(readwrite, copy) NSString *file;
@@ -25,10 +37,6 @@
 @property(readwrite, copy) NSString *name;
 @property(readwrite, copy) NSString *album;
 
-
-
 - (id) initWithPath:(NSString*)path;
 
-// private function
-- (void) updateInfo;
 @end
