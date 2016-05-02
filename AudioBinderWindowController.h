109ee34753674bfcd86f09467566d6b21357f360
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-05-02 16:38:27 -0700

    Minor refactoring

--- AudioBinderWindowController.h
+++ AudioBinderWindowController.h
@@ -1,18 +1,37 @@
 //
-//  AudioBinderWindowController.h
-//  AudioBookBinder
-//
-//  Created by Oleksandr Tymoshenko on 2013-12-14.
-//  Copyright (c) 2013 Bluezbox Software. All rights reserved.
+//  Copyright (c) 2013-2016 Oleksandr Tymoshenko <gonzo@bluezbox.com>
+//  All rights reserved.
+// 
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//  1. Redistributions of source code must retain the above copyright
+//     notice unmodified, this list of conditions, and the following
+//     disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//  SUCH DAMAGE.
 //
 
 #import <Cocoa/Cocoa.h>
+
 #import "AudioFileList.h"
 #import "AudioBinder.h"
 #import "CoverImageView.h"
 
 @interface AudioBinderWindowController : NSWindowController<NSSoundDelegate, NSTextFieldDelegate, AudioBinderDelegate> {
-    
     IBOutlet NSOutlineView *fileListView;
     IBOutlet NSButton *bindButton;
     IBOutlet NSProgressIndicator *fileProgress;
@@ -28,31 +47,9 @@
     IBOutlet NSPanel *saveAsPanel;
     IBOutlet NSTextField *saveAsFilename;
     IBOutlet NSPopUpButton *saveAsFolderPopUp;
-    
-    NSURL *_destURL;
-    NSMutableArray *knownGenres;
-    BOOL autocompleting;
-    BOOL _playing;
-    NSString *outFile;
-    AudioBinder *_binder;
-    NSSound *_sound;
-    NSString *_playingFile;
-    NSImage *_playImg, *_stopImg;
-    BOOL canPlay;
-    NSUInteger currentProgress;
-    BOOL _conversionResult;
-    AudioFileList *fileList;
-    
-    NSMutableArray *currentColumns;
-
-    NSUInteger _currentFileProgress;
-    NSUInteger _totalBookProgress;
-    NSUInteger _totalBookDuration;
-    BOOL _converting;
 }
 
 @property (readwrite, assign) BOOL canPlay;
-
 @property (atomic, assign) NSUInteger currentProgress;
 
 - (IBAction) addFiles: (id)sender;
@@ -65,13 +62,10 @@
 - (IBAction) splitFiles: (id)sender;
 - (IBAction) renumberChapters: (id)sender;
 - (IBAction) playStop: (id)sender;
-
 - (IBAction) saveAsOk:(id)sender;
 - (IBAction) saveAsCancel:(id)sender;
 - (IBAction) folderSheetShow: (id) sender;
 
-- (void)playFailed;
-- (void)sound:(NSSound *)sound didFinishPlaying:(BOOL)finishedPlaying;
 - (void)updateWindowTitle;
 
 @end
--- AudioBinderWindowController.m
+++ AudioBinderWindowController.m
@@ -1,26 +1,45 @@
 //
-//  AudioBinderWindowController.m
-//  AudioBookBinder
-//
-//  Created by Oleksandr Tymoshenko on 2013-12-14.
-//  Copyright (c) 2013 Bluezbox Software. All rights reserved.
+//  Copyright (c) 2013-2016 Oleksandr Tymoshenko <gonzo@bluezbox.com>
+//  All rights reserved.
+// 
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//  1. Redistributions of source code must retain the above copyright
+//     notice unmodified, this list of conditions, and the following
+//     disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//  SUCH DAMAGE.
 //
 
-#import "AudioBookBinderAppDelegate.h"
-#import "AudioBinderWindowController.h"
-#import "Chapter.h"
-#import "AudioBinderVolume.h"
-#import "AudioFile.h"
-#import "MP4File.h"
-#import "ExpandedPathToPathTransformer.h"
-#import "ExpandedPathToIconTransformer.h"
-#include "MetaEditor.h"
 #import "AudioBinder.h"
 #import "AudioBinderVolume.h"
+#import "AudioBinderVolume.h"
+#import "AudioBinderWindowController.h"
+#import "AudioBookBinderAppDelegate.h"
+#import "AudioFile.h"
 #import "Chapter.h"
+#import "Chapter.h"
+#import "ConfigNames.h"
+#import "ExpandedPathToIconTransformer.h"
+#import "ExpandedPathToPathTransformer.h"
+#import "MP4File.h"
+#import "MetaEditor.h"
 #import "NSOutlineView_Extension.h"
 #import "StatsManager.h"
-#import "ConfigNames.h"
 
 // localized strings
 #define TEXT_CONVERSION_FAILED  NSLocalizedString(@"Audiofile conversion failed", nil)
@@ -41,11 +60,11 @@
 #define TEXT_CANCEL             NSLocalizedString(@"Cancel", @"")
 #define TEXT_BOOK_IS_READY      NSLocalizedString(@"Audiobook is ready", @"")
 
-#define ColumnsConfiguration @"ColumnsConfiguration"
+#define ColumnsConfiguration                @"ColumnsConfiguration"
 
-#define KVO_CONTEXT_CANPLAY_CHANGED @"CanPlayChanged"
-#define KVO_CONTEXT_COMMONAUTHOR_CHANGED @"CommonAuthorChanged"
-#define KVO_CONTEXT_COMMONALBUM_CHANGED @"CommonAlbumChanged"
+#define KVO_CONTEXT_CANPLAY_CHANGED         @"CanPlayChanged"
+#define KVO_CONTEXT_COMMONAUTHOR_CHANGED    @"CommonAuthorChanged"
+#define KVO_CONTEXT_COMMONALBUM_CHANGED     @"CommonAlbumChanged"
 
 #ifdef APP_STORE_BUILD
 extern BOOL requiresUpdateHack;
@@ -71,14 +90,33 @@ enum abb_form_fields {
     ABBTitle,
 };
 
-@interface AudioBinderWindowController ()
+@interface AudioBinderWindowController () {
+    NSURL *_destURL;
+    NSMutableArray *knownGenres;
+    BOOL autocompleting;
+    BOOL _playing;
+    NSString *outFile;
+    AudioBinder *_binder;
+    NSSound *_sound;
+    NSString *_playingFile;
+    NSImage *_playImg, *_stopImg;
+    BOOL _conversionResult;
+    AudioFileList *fileList;
+    
+    NSMutableArray *currentColumns;
+
+    NSUInteger _currentFileProgress;
+    NSUInteger _totalBookProgress;
+    NSUInteger _totalBookDuration;
+    BOOL _converting;
+}
+
+- (void)playFailed;
+- (void)sound:(NSSound *)sound didFinishPlaying:(BOOL)finishedPlaying;
 
 @end
 
 @implementation AudioBinderWindowController
-
-@synthesize canPlay, currentProgress;
-
 
 - (id)initWithWindow:(NSWindow *)window
 {
@@ -889,7 +927,7 @@ enum abb_form_fields {
         if (![_sound play]) {
             [playButton setImage:_playImg] ;
             _sound = nil;
-            [playButton setEnabled:canPlay];
+            [playButton setEnabled:self.canPlay];
             [self playFailed];
         }
     }
@@ -899,7 +937,7 @@ enum abb_form_fields {
 {
     [playButton setImage:_playImg] ;
     _sound = nil;
-    [playButton setEnabled:canPlay];
+    [playButton setEnabled:self.canPlay];
 }
 
 - (void) playFailed
