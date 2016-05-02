fa0d0d88ddfa83573dcfc994011e545e82fd41e1
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-05-02 16:32:13 -0700

    Minor refactoring

--- AudioBookBinderAppDelegate.h
+++ AudioBookBinderAppDelegate.h
@@ -1,33 +1,38 @@
 //
-//  AudioBookBinderAppDelegate.h
-//  AudioBookBinder
+//  Copyright (c) 2010-2016 Oleksandr Tymoshenko <gonzo@bluezbox.com>
+//  All rights reserved.
+// 
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//  1. Redistributions of source code must retain the above copyright
+//     notice unmodified, this list of conditions, and the following
+//     disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//  SUCH DAMAGE.
 //
-//  Created by Oleksandr Tymoshenko on 10-02-04.
-//  Copyright 2010 Bluezbox Software. All rights reserved.
-//
+
 
 #import <Cocoa/Cocoa.h>
 
-#ifndef APP_STORE_BUILD
-@class SUUpdater;
-#endif
-
-@interface AudioBookBinderAppDelegate : NSObject<NSWindowDelegate> {
-    IBOutlet NSMenu *applicationMenu;
-
-#ifndef APP_STORE_BUILD
-    IBOutlet SUUpdater *updater;
-#endif
-    
-    NSMutableArray *windowControllers;
-    
-};
+@interface AudioBookBinderAppDelegate : NSObject<NSWindowDelegate>
 
 + (void) initialize;
-
 - (IBAction) openChaptersHowTo: (id)sender;
 - (IBAction) newAudiobookWindow: (id)sender;
-
 #ifndef APP_STORE_BUILD
 - (IBAction) checkForUpdates: (id)sender;
 #endif
--- AudioBookBinderAppDelegate.m
+++ AudioBookBinderAppDelegate.m
@@ -1,10 +1,30 @@
 //
-//  AudioBookBinderAppDelegate.m
-//  AudioBookBinder
+//  Copyright (c) 2010-2016 Oleksandr Tymoshenko <gonzo@bluezbox.com>
+//  All rights reserved.
+// 
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//  1. Redistributions of source code must retain the above copyright
+//     notice unmodified, this list of conditions, and the following
+//     disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+// 
+//  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//  SUCH DAMAGE.
 //
-//  Created by Oleksandr Tymoshenko on 10-02-04.
-//  Copyright 2010 Bluezbox Software. All rights reserved.
-//
+
 
 #import "AudioBookBinderAppDelegate.h"
 #import "ExpandedPathToPathTransformer.h"
@@ -12,7 +32,9 @@
 #import "AudioBinderWindowController.h"
 #import "ConfigNames.h"
 
+#ifndef APP_STORE_BUILD
 #import "Sparkle/SUUpdater.h"
+#endif
 
 #define TEXT_ACTION_REQUIRED    NSLocalizedString(@"User action required", nil)
 
@@ -23,6 +45,18 @@ NSLocalizedString(@"It seems you are upgrading from previous version of Audioboo
 BOOL requiresUpdateHack = NO;
 static BOOL hackChecked = NO;
 #endif
+
+@interface AudioBookBinderAppDelegate() {
+    IBOutlet NSMenu *applicationMenu;
+
+#ifndef APP_STORE_BUILD
+    IBOutlet SUUpdater *updater;
+#endif
+    
+    NSMutableArray *windowControllers;
+}
+
+@end
 
 @implementation AudioBookBinderAppDelegate
 
